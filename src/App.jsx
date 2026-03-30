4c6c41b6792d6c301791522b17d67504927a8f31
hyos <hyozzang99@gmail.com>
2026-03-31 08:42:29 +0900

    useEffect()를 사용하여 원하는 컴포넌트의 state 변경 시 해당 부분만 랜더링 되게 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,33 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
-import Button from './Button'
-import './App.css'
-import styles from './App.module.css'
+import Button from "./Button";
+import "./App.css";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    console.log(counter);
+    setCounter((value) => value + 1)
+  };
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  useEffect(() => {console.log("CALL THE API...")}, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length >= 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue!"}/>
-      <button>normalBtn</button>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
+      <h3>{counter}</h3>
+      <button onClick={onClick}>Click me!</button>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
